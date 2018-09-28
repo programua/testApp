56937cd89c954df5d495db4728d0d7d6f477b69b
programua <thestarsandletmeplayamong@gmail.com>
2018-09-29 00:52:27 +0900

    ・cookie-parser,body-parserの追加 ・uploads.jsのmulterを修正

--- routes/upload.js
+++ routes/upload.js
@@ -2,18 +2,20 @@
 
 let express = require('express');
 let router = express.Router();
-let fs = require('fs');
 let multer = require('multer');
 
-let storage = multer.diskStrage({
+let storage = multer.diskStorage({
     destination: function(req, file, callback){
-        callback(null,'')
+        callback(null, '/files/images')
     },
+    filename: function(req, file, callback){
+        callback(null, file.originalname)
+    }
 });
+let upload = multer({ storage: storage }).single('gafile');
 
-router.post('/', function (req, res) {
-    
-
+router.post('/', upload.single('image'), function (req, res) {
+    res.json({ 'result': 'upload success!' });
 });
 
 module.exports = router;
