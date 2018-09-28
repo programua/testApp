7d0a2fa42ad83b4df04199b545b857b675eb2729
programua <thestarsandletmeplayamong@gmail.com>
2018-09-28 21:15:12 +0900

    multerを使用した画像アップロードに変更

--- routes/upload.js
+++ routes/upload.js
@@ -3,26 +3,17 @@
 let express = require('express');
 let router = express.Router();
 let fs = require('fs');
+let multer = require('multer');
+
+let storage = multer.diskStrage({
+    destination: function(req, file, callback){
+        callback(null,'')
+    },
+});
 
 router.post('/', function (req, res) {
-    let buffers = [];
-    let cnt = 0;
+    
 
-    req.on('data', function (chunk) {
-        buffers.push(chunk);
-        console.log(++cnt);
-    })
-
-    req.on('end', function () {
-        console.log('upload ended');
-        req.rawBody = Buffer.concat(buffers);
-
-        fs.writeFile('../files/images/img.jpeg', req.rawBody, 'utf-8', function (err) { 
-            if(err) return;
-            console.log('image saved');
-        }
-        );
-    });
 });
 
 module.exports = router;
